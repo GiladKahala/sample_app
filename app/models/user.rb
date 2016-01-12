192e0527c0cb8b8ade664a132da607af95107a75
gilad.kahala <gilad.k@fiverr.com>
2016-01-12 17:29:15 -0500

    Add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  has_many :microposts, dependent: :destroy
+
   attr_accessor :remember_token, :activation_token, :reset_token
   attr_accessor :remember_token, :activation_token
   before_save   :downcase_email
@@ -71,6 +73,11 @@ class User < ActiveRecord::Base
     reset_sent_at < 2.hours.ago
   end
 
+  # Defines a proto-feed.
+  # See "Following users" for the full implementation.
+  def feed
+    Micropost.where("user_id = ?", id)
+  end
 
   private
 
